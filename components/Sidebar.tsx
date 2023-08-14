4378255fb954322de6d831a30f0ab35f31db9e6f
unknown <karankartikey72@gmail.com>
2023-08-14 22:52:52 +0530

    added sidebar json and map function

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import Image from "next/image";
 import React, { useState, useEffect } from "react";
+import sidebarItems from "@/mock/menuItems";
 
 const Sidebar = () => {
   const router = useRouter();
@@ -34,27 +35,16 @@ const Sidebar = () => {
     <div className="relative w-full lg:h-screen lg:border-r bg-slate-100 rounded-full lg:rounded-none lg:bg-transparent">
       <div className="lg:absolute">
         <div className="flex flex-row lg:flex-col md:justify-start md:px-6 py-2 gap-y-2 md:gap-y-5">
-          <div
-            className={`py-2 md:px-4 px-2 rounded-full hover:scale-110 duration-300 transition-all text-md md:text-xl ${isLinkActive(
-              "/profile"
-            )}`}
-          >
-            <Link href="/profile">Profile</Link>
-          </div>
-          <div
-            className={`py-2 md:px-4 px-2 rounded-full hover:scale-110 duration-300 transition-all text-md md:text-xl ${isLinkActive(
-              "/my-learnings"
-            )}`}
-          >
-            <Link href="/my-learnings">My Learnings</Link>
-          </div>
-          <div
-            className={`py-2 md:px-4 px-2 rounded-full hover:scale-110 duration-300 transition-all text-md md:text-xl ${isLinkActive(
-              "/payment-history"
-            )}`}
-          >
-            <Link href="/payment-history">Payment History</Link>
-          </div>
+          {sidebarItems.map((item, index) => (
+            <div
+              key={index}
+              className={`py-2 md:px-4 px-2 rounded-full hover:scale-110 duration-300 transition-all text-md md:text-xl ${isLinkActive(
+                item.href
+              )}`}
+            >
+              <Link href={`${item.href}`}>{item.name} </Link>
+            </div>
+          ))}
           <div className="py-2 md:px-4 px-2 rounded-full hover:scale-110 duration-300 transition-all text-md md:text-xl hover:bg-neutral-200">
             <Link href="#">Log out</Link>
           </div>
@@ -65,12 +55,7 @@ const Sidebar = () => {
           className="fixed bottom-4 right-4 p-2 rounded-full bg-gray-500 text-white hover:bg-gray-600 transition-all"
           onClick={scrollToTop}
         >
-          <Image
-            src="/up.svg"
-            alt="up"
-            width={40}
-            height={40}
-          />
+          <Image src="/up.svg" alt="up" width={40} height={40} />
         </button>
       )}
     </div>
---
+++ mock/menuItems.js
@@ -0,0 +1,31 @@
+import { BsLaptop, BsBook } from "react-icons/bs";
+import { IoShirtOutline, IoShirtSharp } from "react-icons/io5";
+import { MdOutlineToys } from "react-icons/md";
+import { RiHeartPulseLine, RiFireLine } from "react-icons/ri";
+import { AiOutlineHome, AiOutlinePercentage } from "react-icons/ai";
+
+const sidebarItems = [
+  {
+    name: "Home",
+    href: "/",
+    icon: AiOutlineHome,
+  },
+  {
+    name: "About",
+    href: "/about",
+    icon: BsPeople,
+  },
+  {
+    name: "Mails",
+    href: "/mails",
+    icon: FiMail,
+  },
+  {
+    name: "Contact",
+    href: "/contact",
+    icon: TiContacts,
+  },
+];
+
+export default sidebarItems;
+
